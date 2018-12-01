5330283baeb7b72e3f7dfd3239590f36a0ed21bc
Rebecca Le <traybaby@gmail.com>
2018-12-01 16:37:40 +0800

    Day 1, part 1 - Initial implementation

---
+++ lib/day_1.ex
@@ -0,0 +1,25 @@
+defmodule Day1 do
+  @doc """
+  iex> Day1.part1(["+1", "+1", "+1"])
+  3
+
+  iex> Day1.part1(["+1", "+1", "-2"])
+  0
+
+  iex> Day1.part1(["-1", "-2", "-3"])
+  -6
+  """
+  def part1(list), do: do_part1(list, 0)
+
+  def do_part1([], result), do: result
+
+  def do_part1([<<sign::binary-1, val::binary>> | rest], result) do
+    new_result = apply(Kernel, String.to_atom(sign), [result, String.to_integer(val)])
+    do_part1(rest, new_result)
+  end
+
+  def parse_input(data) do
+    data
+    |> String.split()
+  end
+end
