3b2abe806c1573c1fbbcc401eab15d9d3b4a5610
Rebecca Le <traybaby@gmail.com>
2018-12-01 16:42:51 +0800

    Day 1, part 1 - A much simpler implementation

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,25 +1,18 @@
 defmodule Day1 do
   @doc """
-  iex> Day1.part1(["+1", "+1", "+1"])
+  iex> Day1.part1("+1\\n+1\\n+1")
   3
 
-  iex> Day1.part1(["+1", "+1", "-2"])
+  iex> Day1.part1("+1\\n+1\\n-2")
   0
 
-  iex> Day1.part1(["-1", "-2", "-3"])
+  iex> Day1.part1("-1\\n-2\\n-3")
   -6
   """
-  def part1(list), do: do_part1(list, 0)
-
-  def do_part1([], result), do: result
-
-  def do_part1([<<sign::binary-1, val::binary>> | rest], result) do
-    new_result = apply(Kernel, String.to_atom(sign), [result, String.to_integer(val)])
-    do_part1(rest, new_result)
-  end
-
-  def parse_input(data) do
+  def part1(data) do
     data
     |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
