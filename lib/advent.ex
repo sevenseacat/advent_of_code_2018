de87d0157afdd6c92d1954092ac60a0df879cdcf
Rebecca Le <traybaby@gmail.com>
2018-12-03 13:36:07 +0800

    Day 3, part 1

--- lib/advent.ex
+++ lib/advent.ex
@@ -12,7 +12,8 @@ defmodule Advent do
       {1, 1, fn -> data(1) |> Day1.part1() end},
       {1, 2, fn -> data(1) |> Day1.part2() end},
       {2, 1, fn -> data(2) |> String.split("\n") |> Day2.part1() end},
-      {2, 2, fn -> data(2) |> String.split("\n") |> Day2.part2() end}
+      {2, 2, fn -> data(2) |> String.split("\n") |> Day2.part2() end},
+      {3, 1, fn -> data(3) |> Day3.part1() end}
     ]
     |> Enum.each(fn {day, part_no, fun} ->
       IO.puts("day #{day}, part #{part_no}: #{Benchmark.measure(fun) |> elem(0)}")
