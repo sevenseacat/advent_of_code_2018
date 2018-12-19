15d137f6b9a92cb37d7c140c0e58036937beb846
Rebecca Le <traybaby@gmail.com>
2018-12-19 16:43:03 +0800

    Day 16, part 1

--- test/advent_test.exs
+++ test/advent_test.exs
@@ -14,6 +14,7 @@ defmodule AdventTest do
   doctest Day11
   doctest Day12
   doctest Day14
+  doctest Day16
 
   describe "day 7, #tick" do
     test "it performs the first tick with sample input" do
